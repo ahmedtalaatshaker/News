5fca77504acd11835967ac616df947d4c19df205
Ahmed Talaat <ahmedtal@banquemisr.com>
2022-11-04 14:06:17 +0200

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Ahmed Talaat on 04/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
